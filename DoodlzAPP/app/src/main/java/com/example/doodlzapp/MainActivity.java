014d3c57317cb23ccf8b50da091f23b6c2e22b1b
TriThuc2321 <71278156+TriThuc2321@users.noreply.github.com>
2021-05-17 22:20:10 +0700

    a

--- DoodlzAPP/app/src/main/java/com/example/doodlzapp/MainActivity.java
+++ DoodlzAPP/app/src/main/java/com/example/doodlzapp/MainActivity.java
@@ -5,12 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Paint;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
 
         /*PaintView paintView = new PaintView(this);
         setContentView(paintView);*/
