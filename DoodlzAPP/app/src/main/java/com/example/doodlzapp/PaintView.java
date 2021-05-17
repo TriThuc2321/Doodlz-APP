66e46314e483d31fa488baf20e3607d32aa8c53b
TriThuc2321 <71278156+TriThuc2321@users.noreply.github.com>
2021-05-17 09:04:36 +0700

    a

---
+++ DoodlzAPP/app/src/main/java/com/example/doodlzapp/PaintView.java
@@ -0,0 +1,54 @@
+package com.example.doodlzapp;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.provider.Settings;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+
+import java.nio.file.Path;
+
+public class PaintView extends View {
+    public ViewGroup.LayoutParams params;
+    private android.graphics.Path path = new android.graphics.Path();
+    private Paint brush = new Paint();
+
+    public PaintView(Context context) {
+        super(context);
+
+        brush.setAntiAlias(true);
+        brush.setColor(Color.BLUE);
+        brush.setStyle(Paint.Style.STROKE);
+        brush.setStrokeJoin(Paint.Join.ROUND);
+        brush.setStrokeWidth(8f);
+
+        params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float pointX = event.getX();
+        float pointY = event.getY();
+
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                path.moveTo(pointX,pointY);
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                path.lineTo(pointX, pointY);
+                break;
+            default: return false;
+        }
+        postInvalidate();
+        return false;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawPath(path, brush);
+    }
+}
